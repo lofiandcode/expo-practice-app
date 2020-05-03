86b0615ef5e3659f9b3aabcd6339061910abe49c
github username <josephtrettevik@gmail.com>
2020-05-02 19:50:20 -0700

    finished Checkbox component

--- components/Checkbox/Checkbox.tsx
+++ components/Checkbox/Checkbox.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Fragment, Component } from 'react';
 import { CheckBox } from 'react-native-elements';
 
-interface  CheckboxProps {
-    checked: boolean;
-    setCheck: Function;
-    size?: number; // default = 18
-    disabled?: boolean;  // default = false
+interface CheckboxProps {
+	checked: boolean;
+	setCheck: Function;
+	size?: number; // default = 18
+	disabled?: boolean;  // default = false
 };
 
-const Checkbox = ({
-    checked,
-    setCheck,
-    size = 18,
-    disabled = false
-}: CheckboxProps) => {
-    <CheckBox 
-        onPress={setCheck} 
-        size={size} 
-        disabled={disabled}
-        checked={checked}
-    />
-}
+class Checkbox extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            checked: false,
+            disabled: true,
+            size: 50
+        }
+    }
 
+    isDisplayed = () => {
+        let checkboxComponent = 
+            <CheckBox 
+                size={this.state.size}
+            />
+    
+        if (this.state.disabled) {
+            checkboxComponent = 
+                <CheckBox size={this.state.size} 
+                    checked={this.state.checked} 
+                    onPress={() => this.setState({checked: !this.state.checked})} 
+                />;
+        } 
+        return (checkboxComponent)
+    } 
+
+    handleOnPress = () => this.setState({checked: !this.state.checked})
+    disabled = false;
+    render() {
+        return (
+            <Fragment>
+                {this.isDisplayed()}
+            </Fragment>
+        )
+    } 
+}
 export default Checkbox;
